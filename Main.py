af90647d2a905101930ddccc3ca7f1829ee6c189
sam rocky <rakisam430@gmail.com>
2019-10-05 05:41:11 +0330

    Fixed All The Problems Ready To Add More Features

--- Main.py
+++ Main.py
@@ -13,33 +13,39 @@ print("{} {}".format(intro, _emoji)) #introducing to user
 #print("")
 tokenizer = WordPunctTokenizer()
 
+try :
+    while True:
+        inp = input("~> ")
+        tokenizedinp = tokenizer.tokenize(inp)
+        if inp is "" : # if 
+            pass
 
-while True:
-    inp = input("~> ")
-    tokenizedinp = tokenizer.tokenize(inp)
-    if inp is "" : # if 
-        pass
-
-    elif inp[1:9] == "TRANSLATE" or " Translate" or "translate" :
-        Content = inp[10:]
-        Dest = input("To what language? Leave empty for using the default ({}) ".format(TDL))
-        if Dest == "" :
-            Dest = langs[TDL]
+        elif (inp[0:9] == "TRANSLATE") or (inp[0:9] == "Translate") or (inp[0:9] == "translate") :
+            Content = inp[10:]
+            Dest = input("To what language? Leave empty for using the default ({}) ".format(TDL))
+            if Dest == "" :
+                Dest = langs[TDL]
+            else :
+                for lang in langs.keys() :
+                    if lang in Dest :
+                        Dest = langs[lang]
+            print(STranslator.Translate(Content, Dest))
         else :
-            for lang in langs.keys() :
-                if lang in Dest :
-                    Dest = langs[lang]
-        print(STranslator.Translate(Content, Dest))
+            try:
+                print(SWikipedia.Compliter(SWikipedia.Wikipedia(inp)))
+                Morecheck = input("Type More to see the whole article ")
+                if ("More" in Morecheck) or ("more" in Morecheck) or ("MORE" in Morecheck) :
+                    ont(SWikipedia.GetURL(inp))
+            except:
+                print("I'm sorry I didn't understand what did you mean, and I couldn't find any match for in Wikipeia\
+                    \nBut in still can search it for you in {}".format(DiffBrow))
+                SearchAsk = input("Do You Want? ")
+                if SearchAsk == "Yes" :
+                    print("Opening Browser ...")
+                    url = "https://www.google.com/search?client=ubuntu&channel=fs&q={}&ie=utf-8&oe=utf-8".format(inp)
+                    ont(url)
 
-    else :
-        try:
-            print(SWikipedia.Compliter(SWikipedia.Wikipedia(inp)))
-            Morecheck = input()
-            if "More" in Morecheck :
-                ont(SWikipedia.GetURL(inp)
-        except:
-            print("I'm sorry I didn't understand what did you mean, and I couldn't find any match for in Wikipeia")
-            SearchAsk = input("Do you want me to search it for you? ")
-            if SearchAsk in YES :
-                print("Sure, Opening Browser")
-                  ont(inp)
+
+except (KeyboardInterrupt):
+    print("\nClosing ...")
+    exit()
