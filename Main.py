08c1480cca0d43b3ae7c320c084a51af20201363
samraki <45524208+samraki@users.noreply.github.com>
2020-05-09 14:27:37 +0430

    Merged it with the local Main.py
    fixed conflicts

--- Main.py
+++ Main.py
@@ -1,35 +1,53 @@
-from Modules import STranslator, SWebscraper, SEmoji, SWikipedia
-from Data import IntroOptions, langs, YES
-from random import choice
-from Setting import *
-from nltk.tokenize import WordPunctTokenizer
+# -*- coding: utf-8 -*-
+""" 
+This is the Main file of Sarah
+#TODO: This Doctype needs changes
+""" 
+
+#Packages and Libraries :
 import re 
+from Modules import STranslator, SWebscraper, SEmoji, SWikipedia, Stts, sayandwrite
+from Data import IntroOptions, langs, YES
+from Setting import *
+#---
+from random import choice
 from webbrowser import open_new_tab as ont
 
-intro = choice(IntroOptions)
-intro_emoji = ["wink", "v", "smile"]
-_emoji = SEmoji.Emoji(choice(intro_emoji)) #making a random emoji
-print("{} {}".format(intro, _emoji)) #introducing to user
-tokenizer = WordPunctTokenizer()
 
+intro = choice(IntroOptions)
+intro_emoji = ["wink", "smile"]
+_emoji = SEmoji.Emoji(choice(intro_emoji)) #making a random emoji
+intro2 = ("{} {}".format(intro, _emoji))
+#Stts.say(intro), Stts.write(intro2)
+#Stts.say(intro)
+Stts.write(intro2)
 
 try :
     while True:
-        inp = input("~> ")
-        tokenizedinp = tokenizer.tokenize(inp)
+        inp = input(Prompt)
         if inp is "" : # if the entered value was empty i'll pass
             pass #passing
         #---
         elif (inp[0:9] == "TRANSLATE") or (inp[0:9] == "Translate") or (inp[0:9] == "translate") :
             Content = inp[10:]
-            Dest = input("To what language? Leave empty for using the default ({}) ".format(TDL))
-            if Dest == "" :
+            Stts.say("To what language?")
+            Stts.write("To what language? Leave empty for using the default ({}) ".format(TDL))
+            Desti = input("> ")
+            if Desti == "" :
+                Desti = TDL
                 Dest = langs[TDL]
             else :
                 for lang in langs.keys() :
-                    if lang in Dest :
+                    if lang in Desti :
                         Dest = langs[lang]
-            print(STranslator.Translate(Content, Dest))
+            translist = STranslator.Translate(Content, Dest)
+            Stts.say("{}, will be, {}, in {}".format(translist[1], translist[2], Desti))
+            Stts.write("{} ~> {} ({})".format(translist[1], translist[2], Desti))
+
+        #---
+        elif (inp == MainName) or (inp == "sarah") :
+            a = ["Yes, I'm here to help you", "Yes, Sir", "I'm right here"]
+            print("{} {}".format(choice(a), _emoji))
         #---
         else :
             try:
@@ -49,5 +67,8 @@ try :
 
 
 except (KeyboardInterrupt):
-    print("\nClosing ...")
-    exit()
+    print("\nSure, Closing ...")
+    try :
+        exit()
+    except:
+        quit()
