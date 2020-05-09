fd26e10a4b9eaa21dead5f4297d7a46bdba445e8
samraki <45524208+samraki@users.noreply.github.com>
2020-05-09 14:33:40 +0430

    Merged it with local Setting.py
    fixed conflicts

--- Setting.py
+++ Setting.py
@@ -1,3 +1,6 @@
 TDL = "Esperanto" #Translation Default Language | Diffault == "Esperanto"
 MainName = "Sarah" #Main App Personal Name That U Can Call | Diffault == "Sarah"
 DiffBrow = "Google" #Diffault Browser for searches | Diffault == "Google" | Other Uptions : DuckDuckGo, Bing, 
+SpeakingAbbility = "on" #Speaking of the assistant | Diffault == "on" | Other Options : off #TODO change to True and False
+WritingAbbility = "on" #Typing of the assistant | Diffault == "on" | Other Options : off ##TODO change to True and False
+Prompt = "~> " #prompt is text or symbols used to represent the system's readiness to perform the next command. | Diffault == "~> "
